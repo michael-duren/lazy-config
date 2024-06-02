795401865c43148abf0af7c40ab082db385968ef
Michael Duren <michaeld@michaelduren.com>
2024-06-02 14:00:09 -0500

    start moving keybindings

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "github/copilot.vim",
+    keys = {
+      {
+        "<leader>Ce",
+        "<cmd>Copilot enable<CR>",
+        silent = false,
+        noremap = true,
+        desc = "Enable copilot",
+      },
+      {
+        "<leader>Cd",
+        "<cmd>Copilot disable<CR>",
+        silent = false,
+        noremap = true,
+        desc = "Disable copilot",
+      },
+      {
+        "<leader>Cs",
+        "<cmd>Copilot status<CR>",
+        silent = false,
+        noremap = true,
+        desc = "Copilot status",
+      },
+    },
+    lazy = false,
+  },
+}
