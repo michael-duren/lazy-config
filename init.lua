3c90470f5169c9165e920e5ca4cae9f403d0bcef
Michael Duren <michaeld@michaelduren.com>
2025-04-10 15:10:40 -0500

    updates

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ require("config.lazy")
 
 vim.cmd("Copilot disable") -- start with copilot disabled
 -- vim.cmd("set hlsearch")
+--
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:block,o:block"
